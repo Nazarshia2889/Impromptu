b4141d0cf22a6ca4324805669d46de3aa0ed46dc
Nathan Dai <nathandai2000@gmail.com>
2024-10-19 21:10:11 -0700

    notes bug fix

--- components/NotesSection.js
+++ components/NotesSection.js
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 
 const NotesSection = ({ notes, suggestions }) => {
+	const [currentNotes, setCurrentNotes] = useState(notes);
+
 	return (
 		<div>
 			<h2 className='text-2xl font-bold mb-4'>Your Notes:</h2>
 			<textarea
 				className='w-full p-3 h-40 border border-gray-300 rounded-md bg-gray-50 resize-none mb-4'
-				value={notes}
+				value={currentNotes}
+				readOnly
 			/>
 			<h2 className='text-2xl font-bold mb-4'>Suggestions:</h2>
 			<ul className='list-disc pl-5'>
